1f1ad9ba3627b33cf60b5e9041a747d5cd35c58d
Vardan Khasikyan <vardan.khasikyan@murka.com>
2024-04-30 00:22:45 +0400

    first commit

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField XDimention;
+        [SerializeField] private TMP_InputField YDimention;
+
+        public void OnSetDeimentions()
+        {
+            
+        }
+    }
+}
